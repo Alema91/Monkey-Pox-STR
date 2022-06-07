9b9e58845140cfe7c657fe6904d6fdfeade2b30a
Alema91 <alberto.lema.blanco@gmail.com>
2022-06-07 13:32:07 +0200

    Libraries and load data

---
+++ _01_analysis.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+# library -------
+
+library(plyr, quietly = TRUE, warn.conflicts = FALSE)
+library(dplyr, quietly = TRUE, warn.conflicts = FALSE)
+library(tidyr, quietly = TRUE, warn.conflicts = FALSE)
+library(tibble, quietly = TRUE, warn.conflicts = FALSE)
+library(readxl, quietly = TRUE, warn.conflicts = FALSE)
+library(stringr, quietly = TRUE, warn.conflicts = FALSE)
+library(readODS, quietly = TRUE, warn.conflicts = FALSE)
+library(readr, quietly = TRUE, warn.conflicts = FALSE)
+library(janitor, quietly = TRUE, warn.conflicts = FALSE)
+library(tibble, quietly = TRUE, warn.conflicts = FALSE)
+library(ggplot2, quietly = TRUE, warn.conflicts = FALSE)
+library(viridis, quietly = TRUE, warn.conflicts = FALSE)
+
+# data --------
