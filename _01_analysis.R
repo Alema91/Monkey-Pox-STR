d9fb62cd63c3c936c6c069563fcca32241f46e8c
Alema91 <alberto.lema.blanco@gmail.com>
2022-06-08 17:58:09 +0200

    ese

--- _01_analysis.R
+++ _01_analysis.R
@@ -7,16 +7,15 @@ library(dplyr, quietly = TRUE, warn.conflicts = FALSE)
 library(tidyr, quietly = TRUE, warn.conflicts = FALSE)
 library(tibble, quietly = TRUE, warn.conflicts = FALSE)
 library(readxl, quietly = TRUE, warn.conflicts = FALSE)
-library(stringr, quietly = TRUE, warn.conflicts = FALSE)
-library(readODS, quietly = TRUE, warn.conflicts = FALSE)
-library(readr, quietly = TRUE, warn.conflicts = FALSE)
 library(janitor, quietly = TRUE, warn.conflicts = FALSE)
 library(tibble, quietly = TRUE, warn.conflicts = FALSE)
 library(ggplot2, quietly = TRUE, warn.conflicts = FALSE)
-library(viridis, quietly = TRUE, warn.conflicts = FALSE)
-library(rgr, quietly = TRUE, warn.conflicts = FALSE)
-library(ggfortify, quietly = TRUE, warn.conflicts = FALSE)
+library(forcats, quietly = TRUE, warn.conflicts = FALSE)
+library(gridExtra, quietly = TRUE, warn.conflicts = FALSE)
+library(patchwork, quietly = TRUE, warn.conflicts = FALSE)
+library(reshape2, quietly = TRUE, warn.conflicts = FALSE)
 
+install.packages("FactoMineR")
 
 # data --------
 
@@ -27,6 +26,10 @@ dir_excel <- list.files(path = "Data", pattern = "xlsx", full.names = TRUE, recu
 ### long encoded ----
 long_full <- read_excel(dir_excel[1], sheet = 1)
 long_encoded <- read_excel(dir_excel[2], sheet = 1)
+
+nombres_df <- as.data.frame(str_split(long_full$Sample_name, "_", simplify = TRUE))
+datos$sequence_nueva <- factor(paste(nombres_df$V1, nombres_df$V2, sep = "_"))
+
 datos <- data.frame(long_encoded[, c(1:3)], long_full[, 2], long_encoded[, c(4)])
 datos$sequence_2 <- as.numeric(as.factor(datos$Sequence_OG))
 datos$sequence_3 <- factor(paste(datos$Sample_name_num, datos$STR_mark, sep = "_"))
@@ -39,9 +42,8 @@ datos$sequence_3 <- factor(paste(datos$Sample_name_num, datos$STR_mark, sep = "_
 # filter_datos<- subset(datos, AlleleFrequency >= 0.5 & STR_mark == "STR9")
 # filter_datos<- subset(datos[,c(2,6,7)], AlleleFrequency >= 0.5)
 filter_datos <- subset(datos[, c(2, 6, 7)])
-head(filter_datos)
-# filter_datos<- subset(datos[,c(2,6,7)])
 
+# filter_datos<- subset(datos[,c(2,6,7)])
 
 # histograma de las frecuencia alelica
 
@@ -51,7 +53,6 @@ ggplot(filter_datos, aes(AlleleFrequency)) +
 # wide table
 
 pivot_table <- pivot_wider(filter_datos, names_from = "sequence_3", values_from = "AlleleFrequency", id_cols = "sequence_2")
-
 
 pivot_table[is.na(pivot_table)] <- 0.0001
 pivot_table$Sequence <- factor(pivot_table$sequence_2)
@@ -65,12 +66,58 @@ modelo_pca1 <- prcomp(t(matrix_table), scale = FALSE)
 plot(modelo_pca1)
 summary(modelo_pca1)
 
+##########################################################################################################################################################
+##########################################################################################################################################################
+##########################################################################################################################################################
+##########################################################################################################################################################
 
+# matrix con los valores de STR y muestras
 
-############ 33
+datos <- read.csv2("Data/xsample_ySTR.tsv", sep = "\t", stringsAsFactors = F)
 
-# Pruebas
+# tengo que cambiar la tabla a long y despues otra vez a wide
 
-df <- iris[1:4]
-data_df <- iris
-str(data_df)
+df_melt <- melt(datos, id.var = c("sequences"))
+df_melt$value <- as.numeric(as.factor(df_melt$value))
+
+df_wide <- pivot_wider(df_melt, names_from = "variable", values_from = "value", id_cols = "sequences")
+
+# Intento de PCA
+
+df_wide <- data.frame(df_wide)
+
+matrix_table <- as.matrix(df_wide[, -1])
+row.names(matrix_table) <- as.character(df_wide[, 1])
+
+modelo_pca1 <- prcomp(matrix_table, scale = FALSE)
+plot(modelo_pca1)
+summary(modelo_pca1)
+
+# Plot PCA
+
+autoplot(modelo_pca1, data = df_wide, loadings.label = TRUE, loadings.label.size = 3, loadings = TRUE, colour = "sequences", shape = T, label = T, label.size = 2.5)
+ggsave("Plots/pca_str_samples.png", width = 18, height = 18, dpi = 300, units = c("cm"))
+
+# heatmap
+
+heatmap(matrix_table)
+ggsave("Plots/heatmap_str_samples.png", width = 18, height = 18, dpi = 300, units = c("cm"))
+
+# intento de hierathical clustering
+
+# dataset
+
+matrix_table <- as.matrix(df_wide[, -1])
+row.names(matrix_table) <- as.character(df_wide[, 1])
+
+# Finding distance matrix
+distance_mat <- dist(matrix_table, method = "minkowski")
+
+# Fitting Hierarchical clustering Model
+# to training dataset
+set.seed(240)
+hier_cl <- hclust(distance_mat, method = "average")
+hier_cl
+
+# Plotting dendrogram
+plot(hier_cl)
