770f260936234d47780a268f627ceb78f79bf233
Alema91 <alberto.lema.blanco@gmail.com>
2022-06-10 12:36:14 +0200

    changes

--- bin/_01_analysis.R
+++ bin/_01_analysis.R
@@ -15,60 +15,9 @@ library(gridExtra, quietly = TRUE, warn.conflicts = FALSE)
 library(patchwork, quietly = TRUE, warn.conflicts = FALSE)
 library(reshape2, quietly = TRUE, warn.conflicts = FALSE)
 library(ggfortify, quietly = TRUE, warn.conflicts = FALSE)
+library(pheatmap, quietly = TRUE, warn.conflicts = FALSE)
 
-# data --------
-
-### Direccion ----
-
-dir_excel <- list.files(path = "Data", pattern = "xlsx", full.names = TRUE, recursive = TRUE, include.dirs = FALSE)
-
-### long encoded ----
-long_full <- read_excel(dir_excel[1], sheet = 1)
-long_encoded <- read_excel(dir_excel[2], sheet = 1)
-
-nombres_df <- as.data.frame(str_split(long_full$Sample_name, "_", simplify = TRUE))
-datos$sequence_nueva <- factor(paste(nombres_df$V1, nombres_df$V2, sep = "_"))
-
-datos <- data.frame(long_encoded[, c(1:3)], long_full[, 2], long_encoded[, c(4)])
-datos$sequence_2 <- as.numeric(as.factor(datos$Sequence_OG))
-datos$sequence_3 <- factor(paste(datos$Sample_name_num, datos$STR_mark, sep = "_"))
-
-# datos$sequence_og_3<- factor(paste(datos$Sample_name_num, datos$STR_mark, datos$sequence_og_2, sep="_"))
-# datos$sequence_og_3<- factor(datos$Sample_name_num)
-
-# filter
-
-# filter_datos<- subset(datos, AlleleFrequency >= 0.5 & STR_mark == "STR9")
-# filter_datos<- subset(datos[,c(2,6,7)], AlleleFrequency >= 0.5)
-filter_datos <- subset(datos[, c(2, 6, 7)])
-
-# filter_datos<- subset(datos[,c(2,6,7)])
-
-# histograma de las frecuencia alelica
-
-ggplot(filter_datos, aes(AlleleFrequency)) +
-    geom_histogram()
-
-# wide table
-
-pivot_table <- pivot_wider(filter_datos, names_from = "sequence_3", values_from = "AlleleFrequency", id_cols = "sequence_2")
-
-pivot_table[is.na(pivot_table)] <- 0.0001
-pivot_table$Sequence <- factor(pivot_table$sequence_2)
-
-# Intento de PCA
-
-matrix_table <- as.matrix(pivot_table[, -1])
-rownames(matrix_table) <- NULL
-
-modelo_pca1 <- prcomp(t(matrix_table), scale = FALSE)
-plot(modelo_pca1)
-summary(modelo_pca1)
-
-##########################################################################################################################################################
-##########################################################################################################################################################
-##########################################################################################################################################################
-##########################################################################################################################################################
+install.packages("d3heatmap")
 
 # matrix con los valores de STR y muestras
 
@@ -123,4 +72,6 @@ hier_cl
 plot(hier_cl)
 
 # plot heatmap
-heatmap(matrix_table)
+png(file = "/home/alberto.lema/Documents/Desarrollo/Monkey-Pox-analysis/plots/heatmap.png", width = 600, height = 350)
+pheatmap(matrix_table, cluster_rows = T, cluster_cols = T)
+dev.off()
