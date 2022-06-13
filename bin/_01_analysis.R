c8f5de5c24d8d4cafe38d58a76db796406f00b8b
Alema91 <alberto.lema.blanco@gmail.com>
2022-06-13 17:34:52 +0200

    Working with several tables and with CLR table. Also, make heatmap each STR

--- bin/_01_analysis.R
+++ bin/_01_analysis.R
@@ -7,7 +7,6 @@ library(dplyr, quietly = TRUE, warn.conflicts = FALSE)
 library(tidyr, quietly = TRUE, warn.conflicts = FALSE)
 library(tibble, quietly = TRUE, warn.conflicts = FALSE)
 library(readxl, quietly = TRUE, warn.conflicts = FALSE)
-library(janitor, quietly = TRUE, warn.conflicts = FALSE)
 library(tibble, quietly = TRUE, warn.conflicts = FALSE)
 library(ggplot2, quietly = TRUE, warn.conflicts = FALSE)
 library(forcats, quietly = TRUE, warn.conflicts = FALSE)
@@ -16,60 +15,76 @@ library(patchwork, quietly = TRUE, warn.conflicts = FALSE)
 library(reshape2, quietly = TRUE, warn.conflicts = FALSE)
 library(ggfortify, quietly = TRUE, warn.conflicts = FALSE)
 library(pheatmap, quietly = TRUE, warn.conflicts = FALSE)
-library(compositions, quietly = TRUE, warn.conflicts = FALSE)
-library(ggfortify, quietly = TRUE, warn.conflicts = FALSE)
+
+# utilidades
+# indx <- sapply(numeric_datos, is.factor)
+# numeric_datos[indx] <- lapply(numeric_datos[indx], function(x) as.numeric(as.character(x)))
+
+# data 01
+mfalleles <- read.csv2("bin/01-most_frequent_alleles_seqs.tsv", sep = "\t", header = T)
+long_mfalleles <- melt(mfalleles, id.var = c("X"))
+colnames(long_mfalleles) <- c("samples", "STR", "Alelles")
+
+# Presence ausence data
+paalelles <- read.csv2("bin/03-all_alleles_presence.tsv", sep = "\t", header = T)
 
 # matrix con los valores de muestras vs STR_alelos
+muestras <- as.character(paalelles[, 1])
+matrix_pa <- as.matrix(paalelles[, -1])
 
-datos <- read.csv2("data/df_prueba_clr_2.csv", sep = "\t", header = T)
-muestras<- as.character(datos[,1])
+# heatmap presencia vs ausencia
+pheatmap(matrix_pa)
 
-numeric_datos<- datos[,-c(1)]
-indx <- sapply(numeric_datos, is.factor)
-numeric_datos[indx] <- lapply(numeric_datos[indx], function(x) as.numeric(as.character(x)))
+# CLR data
+supportclr <- read.csv2("bin/06-all_alleles_supporting_reads_CLR.tsv", sep = "\t", header = T)
+muestras <- as.character(supportclr[, 1])
+matrix_sp <- supportclr[, -1]
 
-ndatos<- data.frame (
-    samples = muestras,
-    numeric_datos
+indx <- sapply(matrix_sp, is.character)
+matrix_sp[indx] <- lapply(matrix_sp[indx], function(x) as.numeric(x))
+row.names(matrix_sp) <- muestras
+
+# realizamos un heatmap por cada STR
+long_sp <- melt(supportclr, id.var = c("X"))
+colnames(long_sp) <- c("samples", "STR", "CLR")
+data_sep_sp <- long_sp %>% separate(STR, c("STR", "alelle", "id"), sep = "_")
+df_sep_sp <- data.frame(
+    sample = data_sep_sp[, 1],
+    STR_alelles = long_sp$STR,
+    STR = data_sep_sp$STR,
+    CLR = data_sep_sp$CLR
 )
 
-# normalizacion por CLR
+id_str <- unique(df_sep_sp$STR)
+rownames(tmp_matrix) <- muestras
+for (i in 1:length(id_str)) {
+    tmp_df <- df_sep_sp[df_sep_sp$STR == id_str[i], ]
+    tmp_df_wide <- data.frame(pivot_wider(tmp_df, names_from = "STR_alelles", values_from = "CLR", id_cols = "sample"))
+    tmp_matrix <- tmp_df_wide[, -1]
+    indx <- sapply(tmp_matrix, is.character) # transform to numeric
+    tmp_matrix[indx] <- lapply(tmp_matrix[indx], function(x) as.numeric(x))
+    nombre_str <- as.character(id_str[i])
+    ruta <- paste0("/home/alberto.lema/Documents/Desarrollo/Monkey-Pox-analysis/plots/", nombre_str, ".png")
+    # heatmap by STR
+    png(file = ruta, width = 600, height = 350)
+    pheatmap(tmp_matrix, show_rownames = T, show_colnames = F)
+    dev.off()
+}
 
-matrix_datos<- as.matrix(numeric_datos)
-clr_datos<- clr(matrix_datos)
-
-df_clr<- data.frame (
-    samples = muestras,
-    clr_datos)
-
-# guardamos los datos transformados
-
-write.table(df_clr, "data/df_clr_str.csv", sep = "\t", col.names = T, quote = F)
-
-# plots con CLR
-
-# heatmap
-
-modelo_pca1 <- prcomp(clr_datos, scale = FALSE)
+# PCA
+modelo_pca1 <- prcomp(matrix_sp, scale = FALSE)
 plot(modelo_pca1)
 summary(modelo_pca1)
 
 # Plot PCA
 
-autoplot(modelo_pca1, data = df_wide, loadings.label = TRUE, loadings.label.size = 3, loadings = TRUE, shape = T, label = T, label.size = 2.5)
+autoplot(modelo_pca1, loadings.label = TRUE, loadings.label.size = 3, loadings = TRUE, shape = T, label = T, label.size = 2.5)
 ggsave("plots/pca_str_samples.png", width = 18, height = 18, dpi = 300, units = c("cm"))
-
-# dataset
-
-row.names(clr_datos) <- df_clr$samples
-
-# Finding distance matrix
-distance_mat <- dist(clr_datos, method = "euclidean")
 
 # Fitting Hierarchical clustering Model
 # to training dataset
 set.seed(240)
-hier_cl <- hclust(distance_mat, method = "average")
+hier_cl <- hclust(matrix_sp, method = "average")
 
 # Plotting dendrogram
 png(file = "C:/Users/alber/Desktop/Desarrollo/Monkey-Pox-STR/plots/hierclust.png", width = 600, height = 350)
